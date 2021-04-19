ad55c891f56e7d6a906613575dcf3b6d094bfa3d
Douglas André <dandrefm@gmail.com>
2021-04-19 16:25:44 -0300

    More updates on test api

--- Gemfile
+++ Gemfile
@@ -29,6 +29,7 @@ gem 'devise', '~> 4.7.0'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
+gem 'dotenv-rails'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token
+  before_action :check_application_token
+
+  def check_application_token
+    return true if request.headers['X-Application-token'] == ENV['APPLICATION_TOKEN']
+    render json: {error: 'Invalid X-Application-token. Please specify a valid token on header' }
+  end
   
 end
